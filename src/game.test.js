daf84a3c04bd7af2934bd887a85b5585f4407c9d
Christopher Lamm <chris@theantichris.com>
2020-08-13 10:56:30 -0400

    underpopulated test

--- src/game.test.js
+++ src/game.test.js
@@ -1,7 +1,7 @@
 import { run } from './game.js'
 
-describe("game of life", () => {
-    it("reproduces on a dead cell with 3 live neighbors", () => {
+describe('game of life', () => {
+    it('reproduces on a dead cell with 3 live neighbors', () => {
         let currentGeneration = [
             [0, 0, 0, 0],
             [0, 0, 1, 0],
@@ -13,6 +13,26 @@ describe("game of life", () => {
             [ 0, 0, 0, 0 ],
             [ 0, 1, 1, 0 ],
             [ 0, 1, 1, 0 ],
+            [ 0, 0, 0, 0 ]
+        ]
+
+        let actual = run(currentGeneration)
+
+        expect(actual).toEqual(expected)
+    })
+
+    it('kills a live cell that is underpopulated', () => {
+        let currentGeneration = [
+            [0, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        let expected = [
+            [ 0, 0, 0, 0 ],
+            [ 0, 0, 0, 0 ],
+            [ 0, 0, 0, 0 ],
             [ 0, 0, 0, 0 ]
         ]
 
