e1a64873c6aa6349d45f37874a6f5dc701d0d830
Christopher Lamm <chris@theantichris.com>
2020-08-13 11:04:26 -0400

    overpopulated test

--- src/game.test.js
+++ src/game.test.js
@@ -40,4 +40,24 @@ describe('game of life', () => {
 
         expect(actual).toEqual(expected)
     })
+
+    it('kills a live cell that is overpopulated', () => {
+        let currentGeneration = [
+            [0, 1, 0, 0],
+            [1, 1, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        let expected = [
+            [ 1, 1, 1, 0 ],
+            [ 1, 0, 1, 0 ],
+            [ 1, 1, 1, 0 ],
+            [ 0, 0, 0, 0 ]
+        ]
+
+        let actual = run(currentGeneration)
+
+        expect(actual).toEqual(expected)
+    })
 })
